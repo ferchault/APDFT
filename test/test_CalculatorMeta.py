27c0f9bab4b6c342de7e64a209f1118f6f597e2f
Guido <guido@vonrudorff.de>
2019-04-10 19:18:17 +0200

    Added horton live test

--- test/test_CalculatorMeta.py
+++ test/test_CalculatorMeta.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pytest
-
+import numpy as np
 import mqm.Calculator as mqmc
 
 def test_force_init():
@@ -9,3 +9,13 @@ def test_force_init():
 
 def test_horton_has_methods():
 	assert 'HF' in mqmc.HortonCalculator._methods.keys()
+
+def test_horton():
+	c = mqmc.HortonCalculator()
+	coordinates = np.array([[0., 0., 0.], [0., 0., 1.]])
+	nuclear_numbers = [1, 1]
+	nuclear_charges = [1., 1.]
+	grid = None
+	method = 'HF'
+	basisset = 'sto3g'
+	c.evaluate(coordinates, nuclear_numbers, nuclear_charges, grid, basisset, method)
