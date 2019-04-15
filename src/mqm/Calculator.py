2c51da031446a14daa7ecff316969a3bdeeea41a
Guido <guido@vonrudorff.de>
2019-04-15 12:49:45 +0200

    Split off horton code

--- src/mqm/Calculator.py
+++ src/mqm/Calculator.py
@@ -13,12 +13,6 @@ import orbkit
 
 class Calculator(object):
 	""" A concurrency-safe blocking interface for an external QM code."""
-	def __init__(self):
-		raise NotImplementedError()
-
-	def evaluate(self, coordinates, nuclear_numbers, nuclear_charges, grid, method, basisset):
-		raise NotImplementedError()
-
 	def get_methods(self):
 		return list(self._methods.keys())
 
@@ -32,8 +26,6 @@ class GaussianCalculator(Calculator):
 		'PBE0': 'PBE1PBE',
 		'PBE': 'PBEPBE',
 	}
-	def __init__(self):
-		pass
 
 	@staticmethod
 	def _format_coordinates(nuclear_numbers, coordinates):
@@ -102,19 +94,6 @@ class GaussianCalculator(Calculator):
 	def get_density_on_grid(self, folder, gridpoints):
 		return GaussianCalculator.density_on_grid(folder + '/run.fchk', gridpoints)
 
-def _horton_setup_hf(obasis, grid, kin, er, na):
-	return [horton.UTwoIndexTerm(kin, 'kin'), horton.UDirectTerm(er, 'hartree'), horton.UExchangeTerm(er, 'x_hf'), horton.UTwoIndexTerm(na, 'ne')]
-
-def _horton_setup_lda(obasis, grid, kin, er, na):
-	return [horton.UTwoIndexTerm(kin, 'kin'), horton.UGridGroup(obasis, grid, [horton.UBeckeHartree(lmax=8), horton.ULibXCLDA('x'), horton.ULibXCLDA('c_vwn')]), horton.UTwoIndexTerm(na, 'ne')]
-
-def _horton_setup_pbe(obasis, grid, kin, er, na):
-	return [horton.UTwoIndexTerm(kin, 'kin'), horton.UDirectTerm(er, 'hartree'), horton.UGridGroup(obasis, grid, [horton.ULibXCGGA('x_pbe'), horton.ULibXCGGA('c_pbe')]), horton.UTwoIndexTerm(na, 'ne')]
-
-def _horton_setup_pbe0(obasis, grid, kin, er, na):
-	libxc_term = horton.ULibXCHybridGGA('xc_pbe0_13')
-	return [horton.UTwoIndexTerm(kin, 'kin'), horton.UDirectTerm(er, 'hartree'), horton.UGridGroup(obasis, grid, [libxc_term]), horton.UExchangeTerm(er, 'x_hf', libxc_term.get_exx_fraction()), horton.UTwoIndexTerm(na, 'ne')]
-
 class HortonCalculator(Calculator):
 	_methods = {
 		'HF': _horton_setup_hf,
@@ -124,51 +103,4 @@ class HortonCalculator(Calculator):
 		}
 
 	def __init__(self):
-		# lazy import only if available
-		global horton
-		import horton
-		horton.log.set_level(0)
-
-	def evaluate(self, coordinates, nuclear_numbers, nuclear_charges, grid, method, basisset):
-		mol = horton.IOData()
-		mol.coordinates = coordinates
-		mol.numbers = nuclear_numbers
-		mol.pseudo_numbers = nuclear_charges
-
-		obasis = horton.get_gobasis(mol.coordinates, nuclear_numbers, basisset)
-
-		olp = obasis.compute_overlap()
-		kin = obasis.compute_kinetic()
-		na = obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers)
-		er = obasis.compute_electron_repulsion()
-
-		orb_alpha = horton.Orbitals(obasis.nbasis)
-		orb_beta = horton.Orbitals(obasis.nbasis)
-
-		one = kin + na
-		horton.guess_core_hamiltonian(olp, one, orb_alpha, orb_beta)
-
-		external = {'nn': horton.compute_nucnuc(mol.coordinates, mol.pseudo_numbers)}
-
-		grid = horton.BeckeMolGrid(mol.coordinates, mol.numbers, mol.pseudo_numbers, 'fine', mode='keep', random_rotate=False)
-
-		terms = self._methods[method](obasis, grid, kin, er, na)
-		ham = horton.UEffHam(terms, external)
-		occ_model = horton.AufbauOccModel(1, 1)
-		occ_model.assign(orb_alpha, orb_beta)
-		dm_alpha = orb_alpha.to_dm()
-		dm_beta = orb_beta.to_dm()
-		scf_solver = horton.EDIIS2SCFSolver(1e-5, maxiter=400)
-		scf_solver(ham, olp, occ_model, dm_alpha, dm_beta)
-
-		fock_alpha = np.zeros(olp.shape)
-		fock_beta = np.zeros(olp.shape)
-		ham.reset(dm_alpha, dm_beta)
-		energy = ham.compute_energy()
-		ham.compute_fock(fock_alpha, fock_beta)
-		orb_alpha.from_fock_and_dm(fock_alpha, dm_alpha, olp)
-		orb_beta.from_fock_and_dm(fock_beta, dm_beta, olp)
-
-		# integration grid
-		rho_alpha = obasis.compute_grid_density_dm(dm_alpha, grid.points)
-		rho_beta = obasis.compute_grid_density_dm(dm_beta, grid.points)
+		pass
--- test/test_CalculatorMeta.py
+++ test/test_CalculatorMeta.py
@@ -3,10 +3,6 @@ import pytest
 import numpy as np
 import mqm.Calculator as mqmc
 
-def test_force_init():
-	with pytest.raises(NotImplementedError):
-		mqmc.Calculator()
-
 def test_horton_has_methods():
 	assert 'HF' in mqmc.HortonCalculator._methods.keys()
 
