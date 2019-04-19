6d531cb1d20e8604db055697052aad5166814c58
Guido <guido@vonrudorff.de>
2019-04-19 09:59:01 +0200

    Test nuclear charges in calculator files

--- test/test_Derivatives.py
+++ test/test_Derivatives.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import pytest
+import glob
+import os
+import shutil
 
 import numpy as np
 
@@ -28,3 +31,36 @@ def test_nucnuc():
 	diff = d.calculate_delta_nuc_nuc(np.array([1, 3]))
 	expected = -0.52917721067
 	assert abs(diff - expected) < 1e-8
+
+def test_filecontents():
+	c = mqmc.GaussianCalculator()
+	d = mqmd.DerivativeFolders(c, 2, [2, 3], np.array([[0, 0, 1], [0, 0, 2]]), 'HF', 'STO-3G')
+	assert d._orders == [0, 1, 2]
+	d.prepare(False)
+
+	def _get_Zs_from_file(fn):
+		with open(fn) as fh:
+			lines = fh.readlines()[-2:]
+		zs = [float(_.split()[-1]) for _ in lines]
+		return zs
+
+	delta = 0.05
+	assert _get_Zs_from_file('multiqm-run/order-0/site-all-cc/run.inp') == [2., 3.]
+
+	assert _get_Zs_from_file('multiqm-run/order-1/site-0-up/run.inp') == [2.+delta, 3.]
+	assert _get_Zs_from_file('multiqm-run/order-1/site-0-dn/run.inp') == [2.-delta, 3.]
+	assert _get_Zs_from_file('multiqm-run/order-1/site-1-up/run.inp') == [2., 3.+delta]
+	assert _get_Zs_from_file('multiqm-run/order-1/site-1-dn/run.inp') == [2., 3.-delta]
+
+	assert _get_Zs_from_file('multiqm-run/order-2/site-0-0-up/run.inp') == [2.+delta, 3.]
+	assert _get_Zs_from_file('multiqm-run/order-2/site-0-0-dn/run.inp') == [2.-delta, 3.]
+	assert _get_Zs_from_file('multiqm-run/order-2/site-1-1-up/run.inp') == [2., 3.+delta]
+	assert _get_Zs_from_file('multiqm-run/order-2/site-1-1-dn/run.inp') == [2., 3.-delta]
+	assert _get_Zs_from_file('multiqm-run/order-2/site-0-1-up/run.inp') == [2.+delta, 3.+delta]
+	assert _get_Zs_from_file('multiqm-run/order-2/site-0-1-dn/run.inp') == [2.-delta, 3.-delta]
+
+	assert set(map(os.path.basename, glob.glob('multiqm-run/*'))) == set('order-0 order-1 order-2'.split())
+	assert set(map(os.path.basename, glob.glob('multiqm-run/order-0/*'))) == set('site-all-cc'.split())
+	assert set(map(os.path.basename, glob.glob('multiqm-run/order-1/*'))) == set('site-0-up site-0-dn site-1-up site-1-dn'.split())
+	assert set(map(os.path.basename, glob.glob('multiqm-run/order-2/*'))) == set('site-0-0-up site-0-0-dn site-1-1-up site-1-1-dn site-0-1-up site-0-1-dn'.split())
+	shutil.rmtree('multiqm-run')
