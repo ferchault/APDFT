36b2ae69ddf8c2179fc0ec2dd4325e7c07c93711
Guido <guido@vonrudorff.de>
2019-04-23 16:02:11 +0200

    Make script importable

--- src/multiQM.py
+++ src/multiQM.py
@@ -12,15 +12,17 @@ parser.add_argument('--remote-preload', help='A command to run on the remote hos
 parser.add_argument('--parallel', type=int, help='Number of parallel executions allowed. If 0, uses all available CPU.')
 parser.add_argument('--do-explicit-reference', action='store_true', help='Whether to do a reference calculation for every target.')
 
-args = parser.parse_args()
 
-calculator = mqm.Calculator.GaussianCalculator()
-nuclear_numbers, coordinates = mqm.read_xyz(args.geometry)
+if __name__ == '__main__':
+	args = parser.parse_args()
 
-derivatives = mqm.Derivatives.DerivativeFolders(calculator, 2, nuclear_numbers, coordinates, args.method, args.basisset)
-derivatives.prepare(args.do_explicit_reference)
-success = derivatives.run(args.parallel, args.remote_host, args.remote_preload)
-if not success:
-	mqm.log.log('Incomplete calculations. Aborting', level='critical')
-	sys.exit(1)
-derivatives.analyse(args.do_explicit_reference)
+	calculator = mqm.Calculator.GaussianCalculator()
+	nuclear_numbers, coordinates = mqm.read_xyz(args.geometry)
+
+	derivatives = mqm.Derivatives.DerivativeFolders(calculator, 2, nuclear_numbers, coordinates, args.method, args.basisset)
+	derivatives.prepare(args.do_explicit_reference)
+	success = derivatives.run(args.parallel, args.remote_host, args.remote_preload)
+	if not success:
+		mqm.log.log('Incomplete calculations. Aborting', level='critical')
+		sys.exit(1)
+	derivatives.analyse(args.do_explicit_reference)
