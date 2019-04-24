b77a308cc3eab14e08a25c370d7ede72d9e52c89
Guido <guido@vonrudorff.de>
2019-04-24 09:27:30 +0200

    Renamed module

--- src/mqm/Physics.py
+++ src/mqm/Physics.py
@@ -11,6 +11,7 @@ class Dipoles(object):
 		""" Calculates the dipole moment of point charges.
 
 		Note that for sets of point charges of a net charge, the resulting dipole moment depends on the chosen reference point. A common choice in the molecular context is the center of mass.
+		Sign convention is such that nuclei should be given as positive charges.
 
 		.. math::
 
@@ -25,3 +26,24 @@ class Dipoles(object):
 		"""
 		shift = coordinates - reference_point
 		return np.sum(shift.T * charges, axis=0) * Dipoles.debye
+
+	@staticmethod
+	def electron_density(reference_point, coordinates, electron_density):
+		""" Calculates the dipole moment of a charge distribution.
+
+		Note that for a charge density, the resulting dipole momennt depends on the chosen reference point. A common choice in the molecular context is the center of mass.
+		Electron density is a positive quantity.
+
+		.. math::
+
+			\mathbf{p}(\mathbf{r}) = \int_\Omega \\rho(\mathbf{r_i})(\mathbf{r_i}-\mathbf{r})
+		
+		Args:
+			reference_point:	A 3 array of the reference point :math:`\mathbf{r}`. [Angstrom]
+			coordinates: 		A (3,N) array of grid coordinates :math:`\mathbf{r_i}`. [Angstrom]
+			electron_density:	A N array of electron density values :math:`\\rho` at `coordinates`. [e/Angstrom^3]
+		Returns:
+			Dipole moment :math:`\mathbf{p}`. [Debye]
+		"""
+		shift = coordinates - reference_point
+		return -np.sum(shift.T * electron_density, axis=0) * Dipoles.debye
--- test/test_Physics.py
+++ test/test_Physics.py
@@ -2,10 +2,20 @@
 import pytest
 import numpy as np
 
-import mqm.Physics as phys
+import mqm.physics as phys
 
 def test_dipole_magnitude():
 	coordinates = np.array([[0., 0., 1.], [0., 0., 0.]])
 	charges = np.array([1, -1])
 	dipole = phys.Dipoles.point_charges(np.array([0., 0., 0.]), coordinates, charges)
 	assert abs(np.linalg.norm(dipole) - 0.20819433) < 1e-7
+
+def test_dipole_sign():
+	coordinates = np.array([[0., 0., 1.], [0., 0., 0.]])
+	charges = np.array([1, -1])
+	dipole = phys.Dipoles.point_charges(np.array([0., 0., 0.]), coordinates, charges)
+	assert dipole[-1] > 0
+
+	charges = np.array([1, 1])
+	dipole = phys.Dipoles.electron_density(np.array([0., 0., 0.]), coordinates, charges)
+	assert dipole < 0
