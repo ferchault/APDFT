Fixed sign tests

src/mqm/Physics.py
@@ -25,7 +25,7 @@ class Dipoles(object):
 			Dipole moment :math:`\mathbf{p}`. [Debye]
 		"""
 		shift = coordinates - reference_point
-		return np.sum(shift.T * charges, axis=0) * Dipoles.debye
+		return np.sum(shift.T * charges, axis=1) * Dipoles.debye
 
 	@staticmethod
 	def electron_density(reference_point, coordinates, electron_density):
@@ -46,4 +46,4 @@ class Dipoles(object):
 			Dipole moment :math:`\mathbf{p}`. [Debye]
 		"""
 		shift = coordinates - reference_point
-		return -np.sum(shift.T * electron_density, axis=0) * Dipoles.debye
+		return -np.sum(shift.T * electron_density, axis=1) * Dipoles.debye

test/test_Physics.py
@@ -16,6 +16,6 @@ def test_dipole_sign():
 	dipole = phys.Dipoles.point_charges(np.array([0., 0., 0.]), coordinates, charges)
 	assert dipole[-1] > 0
 
-	charges = np.array([1, 1])
+	charges = np.array([1, -1])
 	dipole = phys.Dipoles.electron_density(np.array([0., 0., 0.]), coordinates, charges)
-	assert dipole < 0
+	assert dipole[-1] < 0