9bc60e805c6a2520f9f5550cfa01e0167b9c3a3f
Guido <guido@vonrudorff.de>
2019-05-13 15:45:04 +0200

    Reduced number of required calculations

--- src/complementary-isomers.py
+++ src/complementary-isomers.py
@@ -12,7 +12,12 @@ parser = argparse.ArgumentParser(description='Generates complementary isomers.')
 parser.add_argument('geometry', help='An XYZ file with the input molecule.', type=str)
 parser.add_argument('method', help='A QM method.', choices=apdft.get_methods())
 parser.add_argument('basisset', help='A basis set. All of Basis Set Exchange supported.')
-parser.add_argument('directory', help='Output directory, must exist.')
+parser.add_argument('directory', help='Output directory.')
+
+def write_inputfile(path, contents):
+    os.makedirs(path, exist_ok=True)
+    with open(path + '/run.inp', 'w') as fh:
+        fh.write(contents)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -20,10 +25,15 @@ if __name__ == '__main__':
 
     carbons = np.where(nuclear_numbers == 6)[0]
     calc = apdft.Calculator.GaussianCalculator(args.method, args.basisset, superimpose=False)
-    for combination in it.combinations(carbons, 2):
-        thisnumbers = list(nuclear_numbers.copy())
-        mid = calc.get_input(coordinates, thisnumbers, thisnumbers, None)
 
+    # reference energy for comparison purposes
+    thisnumbers = list(nuclear_numbers.copy())
+    mid = calc.get_input(coordinates, thisnumbers, thisnumbers, None)
+    path = '%s/mid' % (args.directory)
+    write_inputfile(path, mid)
+
+    # forward and backward complementary isomer candidates
+    for combination in it.combinations(carbons, 2):
         thisnumbers = list(nuclear_numbers.copy())
         thisnumbers[combination[0]] = 7
         thisnumbers[combination[1]] = 5
@@ -34,8 +44,6 @@ if __name__ == '__main__':
         thisnumbers[combination[1]] = 7
         dn = calc.get_input(coordinates, thisnumbers, thisnumbers, None)
 
-        for direction, inputfile in {'mid': mid, 'up': up, 'dn': dn}.items():
+        for direction, inputfile in {'up': up, 'dn': dn}.items():
             path = '%s/%d-%d-%s' % (args.directory, combination[0], combination[1], direction)
-            os.makedirs(path, exist_ok=True)
-            with open(path + '/run.inp', 'w') as fh:
-                fh.write(inputfile)
+            write_inputfile(path, inputfile)
