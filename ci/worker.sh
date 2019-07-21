ff946c4c0df998e96b51c7e076b6877f1eac4c2b
Guido Falk von Rudorff <guidofalk.vonrudorff@unibas.ch>
2019-07-21 20:56:43 +0200

    Worker script #31

---
+++ ci/worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASEDIR=$(pwd)
+CIDIR=~/_CI
+HEAD=$(git rev-parse HEAD)
+
+source ~/.bashrc
+cd $CIDIR
+rm -rf *
+conda activate ci-env
+
+PATH="$BASEDIR/src:$PATH" python $BASEDIR/ci/ci.py $HEAD
