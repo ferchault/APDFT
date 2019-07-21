a125ab5d3c0e99d6f8e7c88a441258056d522ebf
Guido <guido@vonrudorff.de>
2019-07-21 09:22:59 +0200

    CI runner, #31

--- ci/ci.py
+++ ci/ci.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import subprocess
 
 from github import Github, InputFileContent
 
@@ -30,6 +31,24 @@ def create_issue(repo, url):
 
 def post_status(repo, sha, state, url):
 	repo.get_commit(sha=sha).create_status(state=state, target_url=url, description='Integration tests.', context='continuous-integration/alchemy')
+
+def do_run(method, code):
+	basedir = 'run-%s-%s' % (method, code)
+	os.mkdir(basedir)
+	with open('%s/n2.xyz' % basedir, 'w') as fh:
+		fh.write('2\n\nN 0. 0. 0.\nN 1. 0. 0.')
+	cmds = []
+	cmds.append('python apdft.py --energy_code=%s --energy_method=%s --energy_geometry=n2.xyz')
+	cmds.append('commands.sh')
+	cmds.append('python apdft.py --energy_code=%s --energy_method=%s --energy_geometry=n2.xyz')
+	stdout, stderr = '', ''
+	for cmd in cmds:
+		cp = subprocess.run(cmd.split(), capture_output=True, cwd=basedir)
+		stdout += '\n' + cp.stdout
+		stderr += '\n' + cp.stderr
+		if cp.returncode != 0:
+			return cp.returncode == 0, '\n'.join((stderr, stdout))
+	return cp.returncode == 0, '\n'.join((stderr, stdout))
 
 if __name__ == '__main__':
 	g, repo, sha = connect()
