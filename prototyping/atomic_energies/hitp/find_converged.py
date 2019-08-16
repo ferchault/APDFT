b23df463912f0051ac1eee677a8431a6ddde236c
Michael Sahre <michasahre@gmail.com>
2019-08-16 20:52:59 +0200

    write broken directories to file with error message, so far only for empty fs_cropped

--- prototyping/atomic_energies/hitp/find_converged.py
+++ prototyping/atomic_energies/hitp/find_converged.py
@@ -16,10 +16,17 @@ def get_tree(path):
 
 def find_pp(dirs, dest):
     ready2cube = []
+    broken = []
     for d in dirs:
         status, gemax, num_it = parse_log_file(os.path.join(d,'run.log'))
         if status == 'finished' and gemax < 1e-6:
             ready2cube.append(d)
+        elif status == 'broken':
+            broken.append(d+'\t'+gemax)
     with open(os.path.join(dest, 'ready2cube'), 'w') as fs:
         for item in ready2cube:
+            fs.write("%s\n" % item)
+            
+    with open(os.path.join(dest, 'broken'), 'w') as fs:
+        for item in broken:
             fs.write("%s\n" % item)
