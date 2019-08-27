fe5aa3ac58242b476627c645f2146966b7a43936
Michael Sahre <michasahre@gmail.com>
2019-08-27 18:55:51 +0200

    added more tests

--- prototyping/atomic_energies/tests/test_qml_interface.py
+++ prototyping/atomic_energies/tests/test_qml_interface.py
@@ -12,6 +12,7 @@ import qml_interface as qi
 import unittest
 import numpy as np
 import qml
+import qml.distance
 
 class TestQmlInterface(unittest.TestCase):
 
@@ -68,6 +69,45 @@ class TestQmlInterface(unittest.TestCase):
         full_rep_qi = qi.generate_atomic_representations(alchemy_data, molecule_size)
         # test
         self.assertTrue(np.array_equal(full_matrix, full_rep_qi))
+        
+    def test_calculate_distances(self):
+        # reference data
+        paths = qi.wrapper_alch_data()
+        alchemy_data, molecule_size = qi.load_alchemy_data(paths)
+        full_matrix = qi.generate_atomic_representations(alchemy_data, molecule_size)
+        
+        dist_shape = int(len(full_matrix)*(len(full_matrix)+1)/2)
+        start=0
+        width = len(full_matrix)
+        dist = np.empty(dist_shape)
+        for idx in range(0, len(full_matrix)):
+            dist[start:start+width] = np.linalg.norm(full_matrix[idx]-full_matrix[idx:], axis=1)
+            start = start+width
+            width -= 1
+        
+        # execute
+        dist_qi = qi.calculate_distances(full_matrix)
+        # test
+        self.assertTrue(np.allclose(dist, dist_qi))
+
+    def test_generate_label_vector(self):
+        # reference data
+        paths = qi.wrapper_alch_data()
+        alchemy_data, molecule_size = qi.load_alchemy_data(paths)
+        full_matrix = qi.generate_atomic_representations(alchemy_data, molecule_size)
+
+        energies = np.zeros(len(full_matrix))
+        start = 0
+        for idx, mol in enumerate(alchemy_data):
+            length = len(alchemy_data[idx][:,6])
+            energies[start:length+start] = alchemy_data[idx][:,6]
+            start += length
+            
+        # execute
+        label_qi = qi.generate_label_vector(alchemy_data, molecule_size.sum())
+        # test
+        self.assertTrue(np.array_equal(energies, label_qi))
+
 
 if __name__ == '__main__':
     unittest.main()
