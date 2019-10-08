798994c11b0530aa530114f12ff9ec4d722d6366
Guido <guido@vonrudorff.de>
2019-10-08 17:47:20 +0200

    EPN dimer draft

---
+++ prototyping/pyscf/epn-dimer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import pyscf.gto
+import pyscf.scf
+import pyscf.dft
+import numpy as np
+
+class ElectronicEPN(object):
+    def __init__(self, Z1, Z2, distance):
+        """ Build and cache a dimer (Z1, Z2) at `distance` bond length, given in angstrom. """
+        mol = pyscf.gto.Mole(atom='%d 0 0 0.; %d %f 0 0' % (Z1, Z2, distance), basis='6-31G', verbose=0)
+        mol.build()
+        calc = pyscf.scf.RHF(mol)
+        self._etot = calc.kernel()
+        self._dm = calc.make_rdm1()
+        self._mol = mol
+        
+    def epn(self, pos):
+        """ Electrostatic potential in a.u. at position `pos` given in bohr."""
+        pos = np.array([pos,0,0])
+        self._mol.set_rinv_orig_(pos)
+        return np.matmul(self._dm, self._mol.intor("int1e_rinv")).trace()
+    
+    def density(self, pos):
+        """ Electron density in a.u. at position `pos` given in bohr."""
+        pos = np.array([[pos,0,0]])
+        ao_value = pyscf.dft.numint.eval_ao(self._mol, pos, deriv=0)
+        rho = pyscf.dft.numint.eval_rho(self._mol, ao_value, self._dm, xctype="LDA")
+        return rho[0]
+
+    def total_energy_with_NN(self):
+        """ Total energy including N-N interactions in a.u."""
+        return self._etot
+    
+    def electron_nuclear_interaction(self):
+        """ Interaction energy between electrons and nuclei in a.u."""
+        return sum([self.epn(_[2]) for _ in self._mol.atom_coords()])
