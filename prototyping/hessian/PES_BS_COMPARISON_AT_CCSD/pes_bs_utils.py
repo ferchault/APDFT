5d33f017299477a0d2458b5bf3706cf5eb3cc835
Giorgio <giorgio.domen@gmail.com>
2019-11-14 16:15:06 +0100

    other notebooks for the graphics

--- prototyping/hessian/PES_BS_COMPARISON_AT_CCSD/pes_bs_utils.py
+++ prototyping/hessian/PES_BS_COMPARISON_AT_CCSD/pes_bs_utils.py
@@ -16,6 +16,27 @@ sys.path.append('/home/giorgiod/Documents/APDFT/src')
 from apdft import calculator
 proj_path='/home/giorgiod/MRCC_interface/PES-BS-ALCHEMY/'
 
+# general parse 
+def parse_energy(log_file,calc):
+    calc_str={"HF":"FINAL HARTREE-FOCK ENERGY","ccsd" : "Total CCSD energy", "MP2":"Total MP2 energy [au]",\
+             "PBE":"FINAL KOHN-SHAM ENERGY","B3LYP":"FINAL KOHN-SHAM ENERGY"}
+    """Parse the hartree Fock energy from an MRCC output file"""
+    try:
+        with open(log_file,'r') as logf:
+            while True:
+                line=logf.readline()
+                if calc_str[calc] in line:
+                    good_line=line
+                    for x in good_line.split(' '):
+                        try:
+                            float(x)
+                            return (float(x))
+                        except:
+                            pass    
+    except:
+        print('couldn\'t parse energy return 0 for: '+log_file+'_calc__'+ calc)
+        return 0    
+    
 def parse_energy_cc(log_file):
     """Parse the couple cluster energy from an MRCC output file"""
     try:
@@ -59,6 +80,6 @@ def parse_energy_hf(log_file):
         print('couldn\'t parse energy return 0 for: '+log_file)
         return 0
     
-    
+
 def energy_hf(bs,mol,pt,al=0):
     return parse_energy_hf(dirName(bs,mol,pt,al)+'run.log')
