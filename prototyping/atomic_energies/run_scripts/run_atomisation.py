782dfca3f75f6582ed786169eec232987e3c5adc
Michael Sahre <michasahre@gmail.com>
2019-11-15 20:06:17 +0100

    rewrote function to calculate and save atomisation energies, very similar to write_atomisation in alchemy_tools #139

--- prototyping/atomic_energies/run_scripts/run_atomisation.py
+++ prototyping/atomic_energies/run_scripts/run_atomisation.py
@@ -4,17 +4,63 @@
 Created on Fri Aug 23 16:19:34 2019
 
 @author: misa
+
+calculates the atomisation energies for the compounds specified in dirs
+needs following cube-files: 've_8.cube', 've_15.cube', 've_23.cube', 've_30.cube', 've_38.cube'
 """
 
 import sys
 sys.path.insert(0, '/home/misa/APDFT/prototyping/atomic_energies/')
 sys.path.insert(0, '/home/misa/APDFT/prototyping/atomic_energies/hitp')
 
-from alchemy_tools import write_atomisation_energies
 import os
 os.chdir('/home/misa/APDFT/prototyping/atomic_energies/results/slice_ve38')
+
+import numpy as np
+import re
+
+import alchemy_tools2 as at
+from explore_qml_data import get_free_atom_data
 from find_converged import concatenate_files
 
-dirs = concatenate_files(['/home/misa/APDFT/prototyping/atomic_energies/results/slice_ve38/all_finished_new'])
 
-write_atomisation_energies(dirs)
+
+# paths to the compounds
+dirs = concatenate_files(['/home/misa/APDFT/prototyping/atomic_energies/results/slice_ve38/paths_atomic_energies'])
+
+for compound_path in dirs:
+    # paths to the cube files
+    base = compound_path
+    base = base + 'cube-files/'
+    cubes = ['ve_8.cube', 've_15.cube', 've_23.cube', 've_30.cube', 've_38.cube']
+    for i in range(len(cubes)):
+        cubes[i] = base + cubes[i]
+        
+    path_to_ueg = '/home/misa/APDFT/prototyping/atomic_energies/results/slice_ve38/ueg/ve_00.cube'
+    cubes.insert(0, path_to_ueg)
+    # load 
+    lam_vals, densities, nuclei, gpts, h_matrix = at.load_cube_data(cubes)
+    
+    # atomic energy decomposition
+    nuclei, atomic_energies, alch_pots = at.atomic_energy_decomposition(lam_vals, densities, nuclei, gpts, h_matrix)
+    
+    # calculation of atomisation energy
+    
+    # read total energy B3LYP from xyz-file in qm9 database
+    comp_name = re.search('dsgdb9nsd_......', base)[0] # get the compound name from the path
+    total_energy = at.get_property(os.path.join('/home/misa/datasets/qm9/', comp_name + '.xyz'), 'U0')
+    
+    # read energies B3LYP of free atoms
+    free_atoms_dict = get_free_atom_data()
+    # free atom energy for every atom in compound
+    free_atom_energies = at.get_free_atom_energies(nuclei[:,0], free_atoms_dict)
+    
+    
+    # calculate atomic atomisation energies
+    atomisation_energies = at.calculate_atomisation_energies(atomic_energies, total_energy, free_atom_energies)
+    
+    # write atomic energies and alchemical potentials to file
+    store = np.array([nuclei[:,0], nuclei[:,1], nuclei[:,2], nuclei[:,3], alch_pots, atomic_energies, atomisation_energies]).T
+    header = 'charge\t x_coord\t y_coord\t z_coord\t alchemical_potential\t atomic_energies\t atomisation_energies'
+    save_dir = os.path.join(compound_path, 'atomic_energies_with_mic.txt')
+    np.savetxt(save_dir, store, delimiter='\t', header = header)
