5dddc98f128703cf68c2e416e0f8e57163f75df4
Guido <guido@vonrudorff.de>
2020-02-06 07:48:36 +0100

    numbafied comparison

--- prototyping/networks/build_colored_graphs/list2binary.py
+++ prototyping/networks/build_colored_graphs/list2binary.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
 import numpy as np
-data = np.loadtxt("out.list.gz", dtype=np.int32)
+import tqdm
+import gzip
+import numba
 
-groups = data.sum(axis=1)
-data += 6
-data[data == 8] = 5
+nlines = 1620580590
 
-for nbn in set(groups):
-	np.save("out-nbn-%d" % (nbn/3), data[groups == nbn])
+@numba.njit
+def convert_line(line):
+	data = ""
+	skip = False
+	nbn = 0
+	for element in line[1:]:
+		if skip:
+			skip = False
+			continue
+		skip = True
+		if element == "0": 
+			data += "6 "
+			continue
+		if element == "1": 
+			data += "7 "
+			nbn += 1
+			continue
+		if element == "2": 
+			data += "5 "
+			continue
+	return data[:-1], nbn
+	
+	
+fhs = {}
+for nbn in range(11):
+	fhs[nbn] = open('out-%d' % nbn, 'w', buffering=1024*1024*200)
+	
+for line in tqdm.tqdm(gzip.open("out.list"), total=nlines):
+	line, nbn = convert_line(line.decode('utf-8'))
+	fhs[nbn].write(line + "\n")
+for nbn, fh in fhs.items():
+	fh.close()
