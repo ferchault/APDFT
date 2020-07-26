4558095b7db41a84e229045d50a8b306663e3d4c
Guido <guido@vonrudorff.de>
2020-07-26 20:21:46 +0200

    Support for parallel enumeration

--- prototyping/symmetry-rules/generate.sh
+++ prototyping/symmetry-rules/generate.sh
@@ -3,13 +3,15 @@ NAUTYPATH="/mnt/c/Users/guido/opt/nauty/nauty27rc3"
 NATOMS=$1
 DELTAZMAX=$2
 
+
+LABEL="${NATOMS}-${DELTAMAX}"
 # generate all connected graphs with NATOMS many nodes
-$NAUTYPATH/geng -c -t -f -D3 $NATOMS > graphlist
+$NAUTYPATH/geng -c -t -f -D3 $NATOMS > graphlist-$LABEL
 # remove all but graphs that can be on a hexgrid
-python check-hex-representable.py graphlist > graphlist-hex
+python check-hex-representable.py graphlist-$LABEL > graphlist-hex-$LABEL
 # build all colored graphs
-$NAUTYPATH/vcolg -m$(($DELTAZMAX*2+1)) -T graphlist-hex graphlist-colored
+$NAUTYPATH/vcolg -m$(($DELTAZMAX*2+1)) -T graphlist-hex-$LABEL graphlist-colored-$LABEL
 # filter all non-alchemical symmetry graphs
 echo -e "\n\nEQUATIONS:\n\n"
 # convert graphs into unique equations
-cat graphlist-colored | python filter-alchemy-graphs.py  | python graph-equation.py  | sort -u
+cat graphlist-colored-$LABEL | python filter-alchemy-graphs.py  | python graph-equation.py | sort -u
