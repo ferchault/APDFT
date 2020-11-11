72830b1c43d8b957136cd2d8e2b6b1bf75ab2365
Michael Sahre <michasahre@gmail.com>
2020-11-11 08:21:40 +0100

    #239 and some other stuff maybe

--- prototyping/apdft-ofdft-cpmd/profess_io.py
+++ prototyping/apdft-ofdft-cpmd/profess_io.py
@@ -1,4 +1,18 @@
+import numpy as np
+
 def parse_out_file(file, value_name):
+    # change alias to correct name in file
+    if value_name == 'ekin':
+        value_name = 'Particle   Kinetic   Energy'
+    elif value_name == 'epot':
+        value_name = 'Particle   Potential Energy'
+    elif value_name == 'lattice_vectors':
+        value_name = 'LATTICE INFORMATION'
+    elif value_name == 'temperature':
+        value_name = 'Temperature'
+    elif value_name == 'pressure':
+        value_name = 'Ext Pressure * Vol'
+        
     value = []
     with open(file, 'r') as f:
         for line in f:
@@ -12,6 +26,36 @@ def parse_out_file(file, value_name):
                     value.append(float(line.split()[-2]))
                 elif value_name == 'Final total energy':
                     value = float(line.split()[3])
+                elif value_name == 'Particle   Kinetic   Energy':
+                    value.append(float(line.split(':')[-1]))
+                elif value_name == 'Particle   Potential Energy':
+                    value.append(float(line.split(':')[-1]))
+                elif value_name == 'Temperature':
+                    value.append(float(line.split(':')[-1]))
+                elif value_name == 'Ext Pressure * Vol':
+                    value.append(float(line.split(':')[-1]))
+                elif value_name == 'LATTICE INFORMATION':
+                    lvectors = []
+                    f.readline()
+                    for i in range(3):
+                        a = f.readline()
+                        vector_components = a.split()[2:]
+                        lvectors.append(([float(v) for v in vector_components]))
+                    lvectors = np.array(lvectors)
+                    value.append(lvectors)
+    return(value)
+
+def parse_out_file_MD(file, value_name):
+    value = []
+    with open(file, 'r') as f:
+        for line in f:
+            if not 'ENERGY REPORT' in line:
+                pass
+            else:
+                if value_name == 'temperature':
+                    f.readline()
+                    line = f.readline()
+                    value.append(float(line.split(':')[-1]))
     return(value)
 
 def parse_ion_file(file):
@@ -80,6 +124,9 @@ def parse_force_section(f):
     return(forces)
 
 def generate_cell_section(cell_par):
+    """
+    cell_par: array of lattice vecors [ax, ay, az], np.array([[x,y,z], [x,y,z,], [x,y,z]])
+    """
     cell_section = []
     cell_section.append('%BLOCK LATTICE_CART\n')
     for cell_vec in cell_par:
@@ -107,6 +154,13 @@ def generate_pp_section(elements, pp_names):
     return(pp_section)
     
 def generate_ion_file(cell_par, atom_types, positions, pos_type, pp):
+    """
+    cell_par: array of lattice vecors [ax, ay, az], np.array([[x,y,z], [x,y,z,], [x,y,z]])
+    atom_types: list of element for every atom Len(atom_types)  = number of atoms
+    positions: list of positions
+    pos_type: 'CART' or 'FRAC'
+    pp: pseudo potential file names (one for every element)
+    """
     cell_section = generate_cell_section(cell_par)
     position_section = generate_position_section(atom_types, positions, pos_type)
     elements = set(atom_types)
