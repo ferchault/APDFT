3c52b4e26f6e883b640338610762e640c15d6158
Michael Sahre <michasahre@gmail.com>
2020-11-16 16:27:27 +0100

    #250

--- prototyping/atomic_energies/parse_pyscf.py
+++ prototyping/atomic_energies/parse_pyscf.py
@@ -2,7 +2,7 @@ def read_input(file):
     """
     read input parameters for pyscf calculation
     """
-    input_parameters = {'basis':None, 'intg_meth':None, 'structure_file':None}
+    input_parameters = {'basis':None, 'intg_meth':None, 'structure_file':None, 'lambda':None}
     with open(file, 'r') as f:
         for line in f:
             keyword = line.split()[0]
