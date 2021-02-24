07c0722607eaafe3405be820afe027909018cb4a
michasahre <michasahre@gmail.com>
2021-02-24 10:41:58 +0100

    highthroughput management

--- prototyping/atomic_energies/hitp/get_status_report.py
+++ prototyping/atomic_energies/hitp/get_status_report.py
@@ -4,70 +4,6 @@ import os
 import pandas as pd
 import numpy as np
 
-# check status
-def get_status(workdir):
-    # no log file
-    time = None
-    if os.path.isfile(os.path.join(workdir, 'finished')):
-        # either converged, not converged, broken
-        status, time = get_status_finished(workdir)
-    elif os.path.isfile(os.path.join(workdir, 'submitted')):
-        status = 'submitted'
-    elif os.path.isfile(os.path.join(workdir, 'running')):
-        status = 'running'
-    elif os.path.isfile(os.path.join(workdir, 'run.log')):
-        status, time = get_status_finished(workdir)
-    else:
-        status = 'undefined'
-
-    return(status, time)
-
-def get_status_finished(workdir):
-    # is the latest error file not empty? -> if yes, broken
-    if not error_empty:
-        status = 'broken'
-        time = None
-    else: # read logfile
-        logfile = []
-        with open(os.path.join(workdir, 'run.log'), 'r') as f:
-            logfile = f.readlines()
-        status, time = analyse_logfile(logfile)
-    return(status, time)
-    
-
-    return(last_error_file)
-
-def error_empty(workdir):
-    error_files = glob.glob(os.path.join(workdir, 'error.*'))
-    error_files_ct = []
-    for e in error_files:
-        fname = pathlib.Path(e)
-        error_files_ct.append((fname.stat().st_mtime, fname.stat().st_size))
-    error_files_ct.sort()
-    is_empty = bool(error_files_ct[-1][1] == 0)
-    return(is_empty)
-
-
-def check_convergence(file, linenumber):
-    """
-    if converged9 lines above FINAL RESULTS should be last iteration
-    try to parse this one
-    """
-    success = True
-    try:
-        last_iteration = file[linenumber-8]
-        last_iteration = last_iteration.strip('\n')
-        iteration, conv = int(last_iteration.split()[0]), float(last_iteration.split()[1])
-    except:
-        try:
-            last_iteration = file[linenumber-9]
-            last_iteration = last_iteration.strip('\n')
-            iteration, conv = int(last_iteration.split()[0]), float(last_iteration.split()[1])
-        except:
-            print("Could not parse log-file")
-            success = False
-    return(iteration, conv, success)
-
 def analyse_logfile(logfile):
     status = 'broken'
     # determine status of calculation
@@ -92,24 +28,142 @@ def analyse_logfile(logfile):
         time = None
     return(status, time)
 
-def update(database):
+def check_convergence(file, linenumber):
     """
-    update of status of database entries
+    if converged9 lines above FINAL RESULTS should be last iteration
+    try to parse this one
     """
-    for i in database.index:
-        workdir = database.loc[i]['workdir']
-        #print(workdir)
-        status, time = get_status(workdir)
-        database.loc[i]['status'] = status
-        if time:
-            if np.isnan(database.loc[i]['time']):
-                database.loc[i]['time'] = time
-            else:
-                database.loc[i]['time'] += time
-                
+    success = True
+    try:
+        last_iteration = file[linenumber-8]
+        last_iteration = last_iteration.strip('\n')
+        iteration, conv = int(last_iteration.split()[0]), float(last_iteration.split()[1])
+    except:
+        try:
+            last_iteration = file[linenumber-9]
+            last_iteration = last_iteration.strip('\n')
+            iteration, conv = int(last_iteration.split()[0]), float(last_iteration.split()[1])
+        except:
+            print("Could not parse log-file")
+            success = False
+    return(iteration, conv, success)
+
+def enable_restart(database):
+    # enable restart option
+    workdirs_restart = database.loc[(database['status'] == 'not converged') & (database['restart'] == False), 'workdir']
+    restart_option = []
+    for wd in workdirs_restart:
+        restart_option.append(fix_input_file(wd))
+    database.loc[(database['status'] == 'not converged') & (database['restart'] == False), 'restart'] = restart_option
+
+def error_empty(workdir):
+    """
+    checks if latest error file is empty
+    """
+    error_files = glob.glob(os.path.join(workdir, 'error.*'))
+    error_files.extend(glob.glob(os.path.join(workdir, 'slurm*')))
+    error_files_ct = []
+    for e in error_files:
+        fname = pathlib.Path(e)
+        error_files_ct.append((fname.stat().st_mtime, fname.stat().st_size))
+    error_files_ct.sort()
+    is_empty = bool(error_files_ct[-1][1] == 0)
+    return(is_empty)
+
+def fix_input_file(p, restart_corrupted=False):
+# read input file
+    inp_file = []
+    with open(os.path.join(p, 'run.inp'), 'r') as f:
+        for line in f:
+            inp_file.append(line)
+    # set restart option correctly
+    restart_file = os.path.join(p, 'RESTART.1')
+    restart_exists = os.path.isfile(restart_file)
+    found_restart_option = False
+    restart_enabled = False
+    for i, line in enumerate(inp_file):
+        if 'RESTART WAVEFUNCTION LATEST' in line and restart_exists and not restart_corrupted:
+            found_restart_option = True # everything is ok
+            print(f'Everything ok in {p}')
+            restart_enabled = True
+        # disable restart if there is no restart file
+        if 'RESTART WAVEFUNCTION LATEST' in line and not restart_exists:
+            inp_file[i] = '  rESTART WAVEFUNCTION LATEST\n'
+            found_restart_option = True
+            restart_enabled = False
+            print(f'Disabled restart in {p} because couldnt find a restart file. I hope you dont mind.')
+        # disable restart if there is restart file that is corrupted
+        if 'RESTART WAVEFUNCTION LATEST' in line and restart_corrupted:
+            inp_file[i] = '  rESTART WAVEFUNCTION LATEST\n'
+            found_restart_option = True
+            restart_enabled = False
+            print(f'Disabled restart in {p} because restart file is corrupted. Is that ok with you?')
+        # enable restart if there is a restart file that is not corrupted
+        elif 'rESTART WAVEFUNCTION LATEST' in line and restart_exists and not restart_corrupted:
+            inp_file[i] = '  RESTART WAVEFUNCTION LATEST\n'
+            found_restart_option = True
+            restart_enabled = True
+            print(f'Enabled restart (from rESTART) in {p} because there is an intact restart file. Isn\'t that great, sunshine?')
+    # enable restart if there is a restart file that is not corrupted and restart not added yet
+    if not found_restart_option and restart_exists and not restart_corrupted:
+        for i, line in enumerate(inp_file):
+            if 'OPTIMIZE WAVEFUNCTION' in line:
+                print(f'Added the restart option in {p} because there is an intact restart file. This should help you to finish your PhD in time.')
+                inp_file.insert(i+1, '  RESTART WAVEFUNCTION LATEST\n')
+                restart_enabled = True
+                break
+    
+    # check that PCG MINIMIZE is used
+    # change TIMESTEP to 5 if necessary
+    timestep = False
+    for i, line in enumerate(inp_file):
+        if 'TIMESTEP' in line:
+            timestep = True
+            inp_file[i+1] = '    5\n'
+            if not 'PCG MINIMIZE' in inp_file[i-1]:
+                inp_file.insert(i, '  PCG MINIMIZE\n')
+                print(f'I think you forgot to use the PCG minimizer instead of DIIS in {p}. I changed that for you. Aint I a nice program?')
+    assert timestep
+
+    with open(os.path.join(p, 'run.inp'), 'w') as f:
+        for line in inp_file:
+            f.write(line)
+    return(restart_enabled)
+            
+# check status
+def get_status(workdir):
+    # no log file
+    time = None
+    if os.path.isfile(os.path.join(workdir, 'finished')):
+        # either converged, not converged, broken
+        status, time = get_status_finished(workdir)
+    elif os.path.isfile(os.path.join(workdir, 'submitted')):
+        status = 'submitted'
+    elif os.path.isfile(os.path.join(workdir, 'running')):
+        status = 'running'
+    elif os.path.isfile(os.path.join(workdir, 'run.log')):
+        status, time = get_status_finished(workdir)
+    else:
+        status = 'undefined'
+
+    return(status, time)
+
+def get_status_finished(workdir):
+    # is the latest error file not empty? -> if yes, broken
+    if not error_empty(workdir):
+        status = 'broken'
+        time = None
+    else: # read logfile
+        logfile = []
+        with open(os.path.join(workdir, 'run.log'), 'r') as f:
+            logfile = f.readlines()
+        status, time = analyse_logfile(logfile)
+    return(status, time)
+
 def status_report(db, save_path):
     not_converged = db.loc[db['status']=='not converged', 'workdir']
     broken = db.loc[db['status']=='broken', 'workdir']
+    undefined = db.loc[db['status']=='undefined', 'workdir']
     
     with open(os.path.join(save_path, 'not_converged'), 'w') as f:
         for line in not_converged:
@@ -118,6 +172,10 @@ def status_report(db, save_path):
     with open(os.path.join(save_path, 'broken'), 'w') as f:
         for line in broken:
             f.write(line+'\n')
+        
+    with open(os.path.join(save_path, 'undefined'), 'w') as f:
+        for line in undefined:
+            f.write(line+'\n')
     
     summary = dict()
     for s in ['not converged', 'converged', 'broken', 'submitted', 'running', 'undefined']:
@@ -125,11 +183,33 @@ def status_report(db, save_path):
     with open(os.path.join(save_path, 'status_report'), 'w') as f:
         for k in summary.keys():
             f.write(f"{k}: {summary[k]}\n")
-            
-# load database
-db = pd.read_pickle("/scicore/home/lilienfeld/sahre0000/projects/atomic_energies/results/slice_ve38/calculations_databse")
 
-# update
-update(db)
-# save results
-status_report(db, '/scicore/home/lilienfeld/sahre0000/projects/atomic_energies/results/slice_ve38')
+def update(database):
+    """
+    update of status of database entries
+    """
+    for wd in database['workdir']:
+        #print(workdir)
+        status, time = get_status(wd)
+        database.loc[database['workdir'] == wd ,'status'] = status
+        if time:
+            if np.isnan(database.loc[database['workdir'] == wd, 'time'].item()):
+                database.loc[database['workdir'] == wd , 'time'] = time
+            else:
+                database.loc[database['workdir'] == wd ,'time'] += time
+                              
+if __name__ == "__main__":
+    # load database
+    print('Loading Database...', flush = True)
+    db = pd.read_pickle("/scicore/home/lilienfeld/sahre0000/projects/atomic_energies/results/other_slices/calculation_database.pd")
+    # update
+    print('Updating database...', flush = True)
+    update(db)
+    print('Enabling restart option...', flush = True)
+    enable_restart(db)
+    # save results
+    print('Writing status report...', flush = True)
+    status_report(db, '/scicore/home/lilienfeld/sahre0000/projects/atomic_energies/results/other_slices')
+    print('Saving Database...', flush = True)
+    db.to_pickle("/scicore/home/lilienfeld/sahre0000/projects/atomic_energies/results/other_slices/calculation_database.pd")
+    print('All done! Maybe the restart option should be enabled in some input files.', flush= True)
