f251ad82e175f8a5a23f4379be233dc41c6f65c9
Guido Falk von Rudorff <guido@vonrudorff.de>
2021-03-02 18:08:35 +0100

    Improved graphene lattice generation

--- prototyping/pbc/graphene/formation.py
+++ prototyping/pbc/graphene/formation.py
@@ -1,34 +1,54 @@
 #!/usr/bin/env python
 #%%
-from basis_set_exchange.misc import compact_elements
 import numpy as np
 import shlex
 import scipy.spatial.distance as ssd
 import subprocess
 import json
-import tqdm
 
-BASEDIR = "/mnt/c/Users/guido/workcopies/apdft/prototyping/pbc/graphene"
+XTBPATH = "/home/guido/opt/xtb/xtb-6.4.0/"
 
 
-def get_poscar(scaling, deltaZ):
-    if len(deltaZ) != 128:
+def get_graphene_poscar(scaling, deltaZ, repeat=8, a=2.46):
+    deltaZ = deltaZ.astype(np.int)
+    if len(deltaZ) != 2 * repeat ** 2:
         raise ValueError()
-    print(sum(deltaZ))
     if sum(deltaZ) != 0:
         raise ValueError()
     if len(set(deltaZ) - set((-1, 0, 1))) > 0:
         raise NotImplementedError()
 
-    with open(f"{BASEDIR}/pure-large/POSCAR") as fh:
-        lines = [_.strip() for _ in fh.readlines()]
+    repeat = 8
+    elementstring = ""
+    countstring = ""
+    for element, count in zip("BCN", np.bincount(deltaZ + 1, minlength=3)):
+        if count > 0:
+            elementstring += f" {element}"
+            countstring += f" {count}"
+    header = f"""graphene
+{scaling}
+{a*repeat} 0.0 0.0
+{-a/2*repeat} {a*np.sqrt(3)/2*repeat} 0.0
+0.0 0.0 50
+{elementstring} 
+{countstring}
+Direct
+"""
+    xpts = np.arange(repeat) / repeat
+    A = np.vstack(
+        (np.tile(xpts, repeat), np.repeat(xpts, repeat), np.zeros(64) + 0.5)
+    ).T
+    B = np.vstack((A[:, 0] + 1 / 3 / repeat, A[:, 1] + 2 / 3 / repeat, A[:, 2])).T
+    merged = np.vstack((A, B))
 
-    elements = " ".join(["CNB"[_] for _ in deltaZ])
-    lines[1] = str(scaling)
-    lines[5] = elements + " "  # space needed for reading POSCAR in vmd
-    lines[6] = "1 " * 128
+    # re-sort to match dZ vector
+    merged = np.vstack([merged[deltaZ == _] for _ in (-1, 0, 1)])
 
-    return "\n".join(lines)
+    lines = []
+    for positions in merged:
+        lines.append(f"{positions[0]} {positions[1]} {positions[2]}")
+
+    return header + "\n".join(lines)
 
 
 def random_representative(scaling, nBN):
@@ -38,8 +58,8 @@ def random_representative(scaling, nBN):
     dZ[:nBN] = 1
     dZ[nBN : 2 * nBN] = -1
     np.random.shuffle(dZ)
-    fwd = get_poscar(1, dZ)
-    bwd = get_poscar(1, -dZ)
+    fwd = get_graphene_poscar(1, dZ)
+    bwd = get_graphene_poscar(1, -dZ)
     return fwd, bwd, dZ
 
 
@@ -47,19 +67,29 @@ def random_representative(scaling, nBN):
 
 
 def run_and_extract(poscar):
-    with open(f"{BASEDIR}/rundir/POSCAR", "w") as fh:
+    rundir = "/dev/shm/xtb-atomic-rundir/"
+    try:
+        os.mkdir(rundir)
+    except:
+        pass
+
+    with open(f"{rundir}/POSCAR", "w") as fh:
         fh.write(poscar)
 
-    cmd = "/mnt/c/Users/guido/opt/xtb/6.4.0/bin/xtb --gfn 0 --sp --acc 1e-4 --strict --norestart --json POSCAR"
+    cmd = (
+        f"{XTBPATH}/bin/xtb --gfn 0 --sp --acc 1e-4 --strict --norestart --json POSCAR"
+    )
+    env = os.environ.copy()
+    env["XTBPATH"] = f"{XTBPATH}/share/xtb"
     output = (
         subprocess.check_output(
-            shlex.split(cmd), cwd=f"{BASEDIR}/rundir/", stderr=subprocess.DEVNULL
+            shlex.split(cmd), cwd=rundir, stderr=subprocess.DEVNULL, env=env
         )
         .decode("utf8")
         .split("\n")
     )
-    with open(f"{BASEDIR}/rundir/xtbout.json") as fh:
-        return json.load(fh)["electronic energy"]
+    with open(f"{rundir}/xtbout.json") as fh:
+        return json.load(fh)["total energy"]
 
 
 # region
@@ -78,4 +108,28 @@ data = np.abs(enantiomer_deltas)
 plt.plot(sorted(data), np.arange(len(data)) / len(data))
 data = ssd.pdist(np.array(all_energies).reshape(-1, 1))
 plt.plot(sorted(data), np.arange(len(data)) / len(data))
+# region
+
+
+# region
+# region
+import matplotlib.pyplot as plt
+
+plt.scatter(A[:, 0], A[:, 1])
+plt.scatter(A[:, 0] + 1 / 3 / 8, A[:, 1] + 2 / 3 / 8)
+
+A[:, 0] + 1 / 3 / 8, A[:, 1] + 2 / 3 / 8
+# region
+
+# region
+import os
+
+os.getcwd()
+# region
+dZ = np.zeros(128)
+dZ[0] = -1
+dZ[1] = 1
+print(get_graphene_poscar(1, dZ))
+# region
+print(random_representative(1, 32)[0])
 # region
