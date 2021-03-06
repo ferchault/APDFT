36ada29953c48ca72b3ce4424b6e749b3840d5b0
Guido Falk von Rudorff <guido@vonrudorff.de>
2021-03-06 11:44:06 +0100

    VASP support

--- prototyping/pbc/simple/scan.py
+++ prototyping/pbc/simple/scan.py
@@ -3,7 +3,9 @@
 import subprocess
 import shlex
 import json
+import os
 import numpy as np
+import shutil
 import matplotlib.pyplot as plt
 
 #%%
@@ -22,6 +24,12 @@ DN4 = "Ru Ru Tc Mo Tc Mo"
 
 UP5 = "Cr Cr Mn Fe Mn Fe"
 DN5 = "Fe Fe Mn Cr Mn Cr"
+
+UP6 = "Nb Tc Tc Tc"
+DN6 = "Ru Mo Mo Mo"
+
+UP7 = "Rh Nb Mo Mo"
+DN7 = "Zr Ru Tc Tc"
 
 
 def get_poscar(d, box, elements):
@@ -63,20 +71,62 @@ def run_and_extract(d, box, elements):
     return total - disp - repulsion
 
 
+def run_and_extract(d, box, elements):
+    rundir = "rundir/"
+    shutil.rmtree(rundir)
+    os.mkdir(rundir)
+    with open(f"{rundir}/INCAR", "w") as fh:
+        pass
+
+    with open(f"{rundir}/POSCAR", "w") as fh:
+        fh.write(get_poscar(d, box, elements))
+
+    cmd = "mpirun -np 48  --use-hwthread-cpus /home/guido/opt/vasp/vasp.5.4.4.pl2/bin/vasp_std"
+    env = os.environ
+    env[
+        "LD_LIBRARY_PATH"
+    ] = "/opt/AMD/aocl/aocl-linux-aocc-2.2.0/lib/:/opt/AMD/aocc-compiler-2.3.0/lib/"
+    with open(f"{rundir}/POTCAR", "w") as fhout:
+        for element in elements.split():
+            with open(f"/home/guido/opt/vasp/POT/{element}/POTCAR") as fh:
+                content = fh.read()
+            fhout.write(content)
+    subprocess.check_output(
+        shlex.split(cmd), cwd=rundir, stderr=subprocess.DEVNULL, env=env
+    )
+    with open(f"{rundir}/OUTCAR") as fh:
+        lines = fh.readlines()
+
+    try:
+        TEWEN = float([_ for _ in lines if "TEWEN" in _][-1].strip().split()[-1])
+        TOTEN = float([_ for _ in lines if "TOTEN" in _][-1].strip().split()[-2])
+    except IndexError:
+        print("".join(lines))
+        raise
+
+    print(d, box, elements, TOTEN - TEWEN)
+    return TOTEN - TEWEN
+
+
 def delta(d, box, elements1, elements2):
-    return run_and_extract(d, box, elements1) - run_and_extract(d, box, elements2)
+    try:
+        return run_and_extract(d, box, elements1) - run_and_extract(d, box, elements2)
+    except ValueError:
+        pass
 
 
 #%%
 xs = np.append(np.arange(1, 3, 0.1), np.arange(3, 10, 1))
-ys1_5 = [delta(_, 5, UP, DN) for _ in xs]
-ys1_20 = [delta(_, 20, UP, DN) for _ in xs]
-ys1_50 = [delta(_, 50, UP, DN) for _ in xs]
-ys2_50 = [delta(_, 50, UP2, DN2) for _ in xs]
-ys3_50 = [delta(_, 50, UP3, DN3) for _ in xs]
-ys4_50 = [delta(_, 50, UP4, DN4) for _ in xs]
-ys5_50 = [delta(_, 50, UP5, DN5) for _ in xs]
-np.savetxt("out.txt", np.vstack((xs, ys1_50, ys2_50, ys3_50, ys4_50, ys5_50)).T)
+#ys1_5 = [delta(_, 5, UP, DN) for _ in xs]
+#ys1_20 = [delta(_, 20, UP, DN) for _ in xs]
+# ys1_50 = [delta(_, 50, UP, DN) for _ in xs]
+#ys2_50 = [delta(_, 20, UP2, DN2) for _ in xs]
+#ys3_50 = [delta(_, 20, UP3, DN3) for _ in xs]
+#ys4_50 = [delta(_, 20, UP4, DN4) for _ in xs]
+#ys5_50 = [delta(_, 20, UP5, DN5) for _ in xs]
+ys6_50 = [delta(_, 20, UP6, DN6) for _ in xs]
+ys7_50 = [delta(_, 20, UP7, DN7) for _ in xs]
+#np.savetxt("vaspout.txt", np.vstack((xs, ys1_50, ys2_50, ys3_50, ys4_50, ys5_50)).T)
 print(1 / 0)
 
 # region
@@ -145,4 +195,3 @@ for case in (UP, UP2, UP3, UP4, UP5):
 # region
 for case in (DN, DN2, DN3, DN4, DN5):
     print(get_formation_energy(3, 50, case))
-
