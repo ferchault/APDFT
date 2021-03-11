9bc24fb25d5be4811de60847ea22e07f03712d81
Guido Falk von Rudorff <guido@vonrudorff.de>
2021-03-11 16:06:14 +0100

    k fold parallelization

--- prototyping/pbc/graphene/ml.py
+++ prototyping/pbc/graphene/ml.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 #%%
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import glob
-import tqdm
 import sklearn.metrics as skm
 import qml
+import tqdm
+import contextshare as shared
 
-# region
+#%%
+
+BASEDIR = "/data/guido/graphene-BN/64/up"
+LIMIT = 2500
+
+
 def read_one_log(filename):
     with open(filename) as fh:
         lines = fh.readlines()
@@ -17,15 +24,9 @@ def read_one_log(filename):
         TOTEN = float([_ for _ in lines if "TOTEN" in _][-1].strip().split()[-2])
     except IndexError:
         raise
-
     return TOTEN - TEWEN
 
 
-logfiles = [f"/data/guido/graphene-BN/64/up/{_}/OUTCAR" for _ in range(1, 2501)]
-energies = [read_one_log(_) for _ in tqdm.tqdm(logfiles)]
-# region
-plt.hist(energies)
-# region
 def build_rep(poscar, symmetrize=False):
     # assumptions: xy plane graphene
     with open(poscar) as fh:
@@ -89,136 +90,176 @@ def build_rep(poscar, symmetrize=False):
     return np.vstack((uprep[:64, :], dnrep[64:, :]))
 
 
-molids = range(1, 2501)
-# reps = [
-#    build_rep(f"/data/guido/graphene-BN/64/up/{_}/POSCAR", symmetrize=False)
-#    for _ in tqdm.tqdm(molids)
-# ]
-reps2 = [
-    build_rep(f"/data/guido/graphene-BN/64/up/{_}/POSCAR", symmetrize=True)
-    for _ in tqdm.tqdm(molids)
-]
-# reps
-# reps = np.concatenate(reps)
-# cs, ctrs = build_rep("/data/guido/graphene-BN/64/up/1466/POSCAR")
-# plt.scatter(cs[:, 0], cs[:, 1])
-# b, e = 128*4, 128*5
-# plt.scatter(cs[b:e, 0], cs[b:e, 1])
+def get_KRR_learning_curve_holdout(representations, Y, k, holdoutshare=0.2):
+    ndatapoints = len(Y)
+    totalidx = np.arange(ndatapoints, dtype=np.int)
+    np.random.shuffle(totalidx)
+    holdoutstart = int(len(totalidx) * (1-holdoutshare))
+    maxtrainingset = np.floor(np.log(holdoutstart) / np.log(2))
+    allsigmas = 2.0 ** np.arange(-2, 15)
 
-# region
-def get_KRR_learning_curve(reps, Y, k=5):
-    ns = [128] * len(reps)
-    q = np.concatenate(reps)
-    rows = []
-    totalidx = np.arange(len(Y), dtype=np.int)
-    maxtrainingset = np.floor(np.log(len(Y)) / np.log(2))
-    allsigmas = 2.0 ** np.arange(-2, 10)
-    for sigmaidx, sigma in enumerate(tqdm.tqdm(allsigmas, desc="Sigmas")):
-        Ktotal = qml.kernels.get_local_kernels_gaussian(q, q, ns, ns, [sigma])[0]
+    print ("Calculate kernel matrices")
+    os.environ["OMP_NUM_THREADS"] = str(os.cpu_count())
+    # D = skm.pairwise_distances(representations, n_jobs=-1)
+    ns = np.array([_.shape[0] for _ in representations])
+    representations = np.concatenate(representations)
+    K = qml.kernels.get_local_kernels_gaussian(
+        representations, representations, ns, ns, allsigmas
+    )
+    os.environ["OMP_NUM_THREADS"] = "1"
 
-        for ntrain in 2 ** np.arange(2, maxtrainingset + 1).astype(np.int):
-            maes = {}
-            for fold in range(k):
-                np.random.shuffle(totalidx)
-                train, test = totalidx[:ntrain], totalidx[ntrain:]
+    with shared.SharedMemory(
+        {"K": K, "Y": Y, "totalidx": totalidx[:holdoutstart]}, nworkers=os.cpu_count()
+    ) as sm:
 
-                for lexp in (-9,):
-                    lval = 10 ** lexp
-                    K_subset = Ktotal[np.ix_(train, train)]
-                    K_subset[np.diag_indices_from(K_subset)] += lval
-                    alphas = qml.math.cho_solve(K_subset, Y[train])
+        @sm.register
+        def one_case(sigmaidx, lexp, ntrain, nsplit, chunk):
+            trainvalid = totalidx[:ntrain]
+            Ytrainvalid = Y[trainvalid]
 
-                    K_subset = Ktotal[np.ix_(train, test)]
-                    pred = np.dot(K_subset.transpose(), alphas)
-                    actual = Y[test]
+            # split train into segments
+            width = int(ntrain / nsplit)
+            start = width * chunk
+            end = width * (chunk + 1)
+            if chunk == nsplit:
+                end = ntrain
+            train = np.hstack((trainvalid[:start], trainvalid[end:]))
+            validation = trainvalid[start:end]
+            Y_train = np.hstack((Ytrainvalid[:start], Ytrainvalid[end:]))
+            Y_validation = Ytrainvalid[start:end]
 
-                    thismae = np.abs(pred - actual).mean()
-                    if lexp not in maes:
-                        maes[lexp] = []
-                    maes[lexp].append(thismae)
+            # build model
+            lval = 10 ** lexp
+            K_subset = K[sigmaidx][np.ix_(train, train)]
+            K_subset[np.diag_indices_from(K_subset)] += lval
+            alphas = np.linalg.solve(K_subset, Y_train)
 
-            for lexp in maes.keys():
-                mae = np.average(maes[lexp])
-                std = np.std(maes[lexp])
-                rows.append(
-                    {
-                        "sigma": sigma,
-                        "lexp": lexp,
-                        "ntrain": ntrain,
-                        "mae": mae,
-                        "std": std,
-                    }
-                )
+            # evaluate model
+            K_subset = K[sigmaidx][np.ix_(train, validation)]
+            pred = np.dot(K_subset.transpose(), alphas)
 
-    rows = pd.DataFrame(rows)
-    ns, maes, stds = [], [], []
-    for name, group in rows.groupby("ntrain"):
-        bestcase = group.sort_values("mae")
-        ns.append(name)
-        maes.append(bestcase["mae"].values[0])
-        stds.append(bestcase["std"].values[0])
-    ns = np.array(ns)
-    maes = np.array(maes)
-    stds = np.array(stds)
-    order = np.argsort(ns)
-    return ns[order], maes[order], stds[order]
+            mae = np.abs(pred - Y_validation).mean()
+            return {
+                "sigmaidx": sigmaidx,
+                "lexp": lexp,
+                "ntrain": ntrain,
+                "chunk": chunk,
+                "mae": mae,
+            }
+
+        for sigmaidx in range(len(allsigmas)):
+            for lexp in (-7, -9, -11, -13):
+                for ntrain in 2 ** np.arange(6, maxtrainingset + 1).astype(np.int):
+                    for fold in range(k):
+                        one_case(sigmaidx, lexp, ntrain, k, fold)
+        print ("Hyperparameter optimization")
+        foldresults = pd.DataFrame(sm.evaluate(progress=True))
+    
+    with shared.SharedMemory(
+        {"K": K, "Y": Y, "totalidx": totalidx}, nworkers=os.cpu_count()
+    ) as sm:
+        @sm.register
+        def other_case(sigmaidx, lexp, ntrain, holdoutstart):
+            train = totalidx[:ntrain]
+            holdout = totalidx[holdoutstart:]
+
+            # build model
+            lval = 10 ** lexp
+            K_subset = K[sigmaidx][np.ix_(train, train)]
+            K_subset[np.diag_indices_from(K_subset)] += lval
+            alphas = np.linalg.solve(K_subset, Y[train])
+
+            # evaluate model
+            K_subset = K[sigmaidx][np.ix_(train, holdout)]
+            pred = np.dot(K_subset.transpose(), alphas)
+
+            return np.abs(pred - Y[holdout]).mean()
+
+        params = foldresults.groupby("sigmaidx lexp ntrain".split()).mean()['mae'].reset_index().sort_values("ntrain")
+        ns = []
+        for ntrain, group in params.groupby("ntrain"):
+            loc = group.sort_values("mae").iloc[0]
+            ns.append(ntrain)
+            other_case(int(loc.sigmaidx), int(loc.lexp), ntrain, holdoutstart)
+        print ("Build models")
+        maes = sm.evaluate(progress=True)
+        return ns, maes
+
+if __name__ == "__main__":
+    logfiles = [f"{BASEDIR}/{_}/OUTCAR" for _ in range(1, LIMIT + 1)]
+    energies = np.array([read_one_log(_) for _ in logfiles])
+    molids = range(1, LIMIT + 1)
+    with shared.SharedMemory({}, nworkers=os.cpu_count()) as sm:
+
+        @sm.register
+        def load_rep(molid):
+            return build_rep(f"{BASEDIR}/{molid}/POSCAR", symmetrize=False)
+
+        for i in molids:
+            load_rep(i)
+
+        print ("Build representations")
+        reps = sm.evaluate(progress=True)
+        reps = np.array(reps)
+    # reps2 = [build_rep(f"{BASEDIR}/{_}/POSCAR", symmetrize=True) for _ in molids]
+    q = get_KRR_learning_curve_holdout(reps, energies, 10)
+    print (q)
 
 
 # ns, maes, stds = get_KRR_learning_curve(reps, np.array(energies), k=10)
 # print(ns, maes, stds)
-ns2, maes2, stds2 = get_KRR_learning_curve(reps2, np.array(energies), k=10)
-print(ns2, maes2, stds2)
-# region
+# ns2, maes2, stds2 = get_KRR_learning_curve(reps2, np.array(energies), k=10)
+# print(ns2, maes2, stds2)
+# %%
 # plt.loglog(ns, maes, "o-", label="orig")
 # plt.loglog(ns2, maes2, "o-", label="with symmetry")
-# plt.legend()
-# region
-
+# reps.shape
+#df = pd.DataFrame(q)
 #%%
-xs = (4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048)
-yswithsymm = np.array(
-    (
-        91.38916025,
-        69.94414159,
-        56.62984254,
-        41.5187096,
-        32.99853503,
-        25.53158878,
-        18.82178703,
-        9.95941045,
-        6.69857606,
-        5.45049415,
-    )
-)
-ysorig = np.array(
-    (
-        106.71834446,
-        83.70406486,
-        67.0399434,
-        44.90289698,
-        33.4512471,
-        27.73255691,
-        24.38229124,
-        14.79122741,
-        10.28452817,
-        7.93315388,
-    )
-)
-plt.loglog(xs, ysorig / 128 * 1000, "o-", color="C0", label="atomic CM w/cutoff")
-plt.loglog(
-    xs[:-1],
-    ysorig[1:] / 128 * 1000,
-    "o-",
-    color="C0",
-    alpha=0.5,
-    label="expected with symmetry",
-)
-plt.loglog(
-    xs, yswithsymm / 128 * 1000, "s-", color="C1", label="same + alchemical symmetry"
-)
-plt.yticks((40, 60, 80, 100, 200, 400, 800), (40, 60, 80, 100, 200, 400, 800))
-plt.xticks(xs, xs)
-plt.ylabel("meV / atom")
-plt.legend()
-plt.xlabel("Training set size")
-# region
+# xs = (4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048)
+# yswithsymm = np.array(
+#     (
+#         91.38916025,
+#         69.94414159,
+#         56.62984254,
+#         41.5187096,
+#         32.99853503,
+#         25.53158878,
+#         18.82178703,
+#         9.95941045,
+#         6.69857606,
+#         5.45049415,
+#     )
+# )
+# ysorig = np.array(
+#     (
+#         106.71834446,
+#         83.70406486,
+#         67.0399434,
+#         44.90289698,
+#         33.4512471,
+#         27.73255691,
+#         24.38229124,
+#         14.79122741,
+#         10.28452817,
+#         7.93315388,
+#     )
+# )
+# plt.loglog(xs, ysorig / 128 * 1000, "o-", color="C0", label="atomic CM w/cutoff")
+# plt.loglog(
+#     xs[:-1],
+#     ysorig[1:] / 128 * 1000,
+#     "o-",
+#     color="C0",
+#     alpha=0.5,
+#     label="expected with symmetry",
+# )
+# plt.loglog(
+#     xs, yswithsymm / 128 * 1000, "s-", color="C1", label="same + alchemical symmetry"
+# )
+# plt.yticks((40, 60, 80, 100, 200, 400, 800), (40, 60, 80, 100, 200, 400, 800))
+# plt.xticks(xs, xs)
+# plt.ylabel("meV / atom")
+# plt.legend()
+# plt.xlabel("Training set size")
+# %%
