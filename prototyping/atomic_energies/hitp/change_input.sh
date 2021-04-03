d009b3ac72129076ff6166a1cc48f1292bc67e1f
michasahre <michasahre@gmail.com>
2021-04-03 22:41:40 +0200

    removed udated files

--- prototyping/atomic_energies/hitp/change_input.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for directory in $(cat r4); do
-#    sed -i '/\s\sOPTIMIZE WAVEFUNCTION/a \s\sRESTART WAVEFUNCTION LATEST'  $directory"run.inp"
-#    sed -i '/\s\sRESTART WAVEFUNCTION LATEST/a \  PCG MINIMIZE'  $directory"run.inp"
-    sed -i '/\s\sTIMESTEP/!b;n;c \    0\.005' $directory"run.inp"
-done
