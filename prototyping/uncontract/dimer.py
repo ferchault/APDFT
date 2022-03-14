fd4e392775a48c3583f1c325c61697f24cee4e73
von Rudorff, Guido Falk <guido@vonrudorff.de>
2022-03-14 16:38:08 -0700

    Some docs

--- prototyping/uncontract/dimer.py
+++ prototyping/uncontract/dimer.py
@@ -7,6 +7,7 @@ import numpy as np
 # %%
 
 def add_qmmm(calc, mol, deltaZ):
+    """ Alters the effective nuclear charges by adding point charges on top of atoms."""
     angstrom = 1 / 0.52917721067
     mf = pyscf.qmmm.mm_charge(calc, mol.atom_coords() / angstrom, deltaZ)
 
@@ -21,6 +22,7 @@ def add_qmmm(calc, mol, deltaZ):
 
 
 def get_N2_energy(dZ, basis):
+    """ Calculates the electronic N2 energy with changes in nuclear charges dZ in the given basis"""
     mol = pyscf.gto.M(atom="N 0 0 0; N 0 0 1", basis=basis)
     mol.verbose = 0
     mf = pyscf.scf.RHF(mol)
@@ -29,6 +31,7 @@ def get_N2_energy(dZ, basis):
 
 
 def get_3rd_order_CO_estimate(basis):
+    """ Finite differences for up to including 3rd order."""
     delta = 0.001
     dlambda = [_ * delta for _ in range(-2, 3)]
     energies = [get_N2_energy((_, -_), basis) for _ in dlambda]
@@ -41,12 +44,15 @@ def get_3rd_order_CO_estimate(basis):
     return zeroth+first+second/2+third/6
 
 def get_true_CO_estimate(basis):
+    """ CO electronic energy in the basis set of N2."""
     return get_N2_energy((1, -1), basis)
 
 def get_error_reference_basis(basis):
+    """ Compares prediction to SCF in N2 basis set."""
     return (get_3rd_order_CO_estimate(basis) - get_true_CO_estimate(basis))*1000
 
 def get_error_target_basis(basisname, basis):
+    """ Compares prediction to SCF in CO basis set."""
     mol = pyscf.gto.M(atom="C 0 0 0; O 0 0 1", basis=basisname)
     mol.verbose = 0
     calc = pyscf.scf.RHF(mol)
